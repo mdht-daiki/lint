2956c2a071a4c7258a9839ff2d4fa91ebade3cb9
mdht-daiki <70727838+mdht-daiki@users.noreply.github.com>
2020-10-01 16:46:31 +0900

    配列から文字列への変換実装

--- main.c
+++ main.c
@@ -13,17 +13,6 @@ void trim_nl(char *s) {
   }
 }
 
-/* 文字列をint型配列に変換する */
-int *string_to_lint(char* S) {
-  int N = strlen(S);
-  int *lint = (int *)malloc(sizeof(int)*(N+1));
-  for(int i = 0; i < N; i++) {
-    lint[i] = S[N - i - 1] - '0';
-  }
-  lint[N] = -1;
-  return lint;
-}
-
 /* 文字列がint型配列に変換できるものか確認する */
 int check_string(char* S) {
   int N = strlen(S);
@@ -34,6 +23,35 @@ int check_string(char* S) {
     }
   }
   return 1;
+}
+
+/* int型配列の要素数をカウントする */
+int lint_length(int* lint) {
+  int length = 0;
+  do { 
+    length++;
+  } while(lint[length] != -1);
+  return length;
+}
+
+/* 文字列をint型配列に変換する */
+int *string_to_lint(char* S) {
+  int N = strlen(S);
+  int *lint = (int *)malloc(sizeof(int)*(N+1));
+  for(int i = 0; i < N; i++) {
+    lint[i] = S[N - i - 1] - '0';
+  }
+  lint[N] = -1;   /* 番兵 */
+  return lint;
+}
+
+/* int型配列を文字列に変換する */
+void lint_to_string(int *lint, char *ans) {
+  int length = lint_length(lint);
+  for(int i = 0; i < length; i++) {
+    ans[i] = lint[length - i - 1] + '0';
+  }
+  ans[length] = '\0';
 }
 
 int main(void) {
@@ -55,6 +73,9 @@ int main(void) {
   for(i = 0; i < n; i++) {
       for(j = 0; lint[i][j] != -1; j++)
         printf("lint[%d][%d] = %d\n", i, j, lint[i][j]);
+      printf("length = %d\n", lint_length(lint[i]));
+      lint_to_string(lint[i], buf);
+      printf("lint[%d] = %s\n", i, buf);
   }
 
   for(i = 0; i < n; i++)
